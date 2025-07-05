8e1e4242981b6d231832166c66e3ed175b9e3dfc
dagargo <dagargo@gmail.com>
2025-07-05 15:58:37 +0200

    EIII: Rewrite tests
    As the file name lenght restriction is gone, there is no limitation
    of the bank name length and emu4 tests can be written using clear
    names. Banks used for emu3 need to be rewritten though.

--- test/emu3_test_add_zone.sh
+++ test/emu3_test_add_zone.sh
@@ -2,7 +2,7 @@
 
 . $srcdir/test_common.sh
 
-TEST_BANK_NAME=$srcdir/test_add_zone
+TEST_BANK_NAME=$srcdir/emu3_test_add_zone
 
 cleanUp
 
@@ -14,27 +14,27 @@ testError
 
 logAndRun '$srcdir/../src/emu3bm -p "P0" $TEST_BANK_NAME'
 test
-logAndRun 'diff $TEST_BANK_NAME data/test_add_zone_1'
+logAndRun 'diff $TEST_BANK_NAME data/emu3_test_add_zone_1'
 test
 
 logAndRun '$srcdir/../src/emu3bm -s data/s1.wav $TEST_BANK_NAME'
 test
-logAndRun 'diff $TEST_BANK_NAME data/test_add_zone_2'
+logAndRun 'diff $TEST_BANK_NAME data/emu3_test_add_zone_2'
 test
 
 logAndRun '$srcdir/../src/emu3bm -e 0 -z 1,pri,F1,C1,B1 $TEST_BANK_NAME'
 test
-logAndRun 'diff $TEST_BANK_NAME data/test_add_zone_3'
+logAndRun 'diff $TEST_BANK_NAME data/emu3_test_add_zone_3'
 test
 
 logAndRun '$srcdir/../src/emu3bm -e 0 -z 1,pri,F2,C2,B2 $TEST_BANK_NAME'
 test
-logAndRun 'diff $TEST_BANK_NAME data/test_add_zone_4'
+logAndRun 'diff $TEST_BANK_NAME data/emu3_test_add_zone_4'
 test
 
 logAndRun '$srcdir/../src/emu3bm -e 0 -z 1,sec,f1,c1,b1 $TEST_BANK_NAME'
 test
-logAndRun 'diff $TEST_BANK_NAME data/test_add_zone_5'
+logAndRun 'diff $TEST_BANK_NAME data/emu3_test_add_zone_5'
 test
 
 cleanUp
@@ -44,34 +44,34 @@ test
 
 logAndRun '$srcdir/../src/emu3bm -p "P0" $TEST_BANK_NAME'
 test
-logAndRun 'diff $TEST_BANK_NAME data/test_add_zone_1'
+logAndRun 'diff $TEST_BANK_NAME data/emu3_test_add_zone_1'
 test
 
 logAndRun '$srcdir/../src/emu3bm -s data/s1.wav $TEST_BANK_NAME'
 test
-logAndRun 'diff $TEST_BANK_NAME data/test_add_zone_2'
+logAndRun 'diff $TEST_BANK_NAME data/emu3_test_add_zone_2'
 test
 
 logAndRun '$srcdir/../src/emu3bm -e 0 -Z 1,pri,20,15,26 $TEST_BANK_NAME'
 test
-logAndRun 'diff $TEST_BANK_NAME data/test_add_zone_3'
+logAndRun 'diff $TEST_BANK_NAME data/emu3_test_add_zone_3'
 test
 
 logAndRun '$srcdir/../src/emu3bm -e 0 -Z 1,pri,32,27,38 $TEST_BANK_NAME'
 test
-logAndRun 'diff $TEST_BANK_NAME data/test_add_zone_4'
+logAndRun 'diff $TEST_BANK_NAME data/emu3_test_add_zone_4'
 test
 
 logAndRun '$srcdir/../src/emu3bm -e 0 -Z 1,sec,20,15,26 $TEST_BANK_NAME'
 test
-logAndRun 'diff $TEST_BANK_NAME data/test_add_zone_5'
+logAndRun 'diff $TEST_BANK_NAME data/emu3_test_add_zone_5'
 test
 
 logAndRun '$srcdir/../src/emu3bm -e 0 --delete-zone 0 $TEST_BANK_NAME'
 test
 logAndRun '$srcdir/../src/emu3bm -e 0 --delete-zone 0 $TEST_BANK_NAME'
 test
-logAndRun 'diff $TEST_BANK_NAME data/test_add_zone_2'
+logAndRun 'diff $TEST_BANK_NAME data/emu3_test_add_zone_2'
 test
 
 cleanUp
--- test/emu3_test_create_bank.sh
+++ test/emu3_test_create_bank.sh
@@ -2,27 +2,27 @@
 
 . $srcdir/test_common.sh
 
-TEST_BANK_NAME=$srcdir/test_create_bank
+TEST_BANK_NAME=$srcdir/emu3_test_create_bank
 
 cleanUp
 
 logAndRun '$srcdir/../src/emu3bm -n $TEST_BANK_NAME'
 test
-logAndRun 'diff $TEST_BANK_NAME $srcdir/data/test_create_bank_esi2000'
+logAndRun 'diff $TEST_BANK_NAME $srcdir/data/emu3_test_create_bank_esi2000'
 test
 
 cleanUp
 
 logAndRun '$srcdir/../src/emu3bm -d esi2000 -n $TEST_BANK_NAME'
 test
-logAndRun 'diff $TEST_BANK_NAME $srcdir/data/test_create_bank_esi2000'
+logAndRun 'diff $TEST_BANK_NAME $srcdir/data/emu3_test_create_bank_esi2000'
 test
 
 cleanUp
 
 logAndRun '$srcdir/../src/emu3bm -d emu3x -n $TEST_BANK_NAME'
 test
-logAndRun 'diff $TEST_BANK_NAME $srcdir/data/test_create_bank_emu3x'
+logAndRun 'diff $TEST_BANK_NAME $srcdir/data/emu3_test_create_bank_emu3x'
 test
 
 cleanUp
