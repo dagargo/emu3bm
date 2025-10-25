ade01d84edaa2d3fad7568b50bf7cc93417aafb4
dagargo <dagargo@gmail.com>
2025-10-25 12:37:32 +0200

    Fix emu4 extract samples test

--- test/emu4_test_extract_samples.sh
+++ test/emu4_test_extract_samples.sh
@@ -17,19 +17,19 @@ cd $EXT_DIR
 logAndRun '$srcdir/../../src/emu4bm -x ../data/emu4_test_add_sample_2'
 test
 
-logAndRun 'diff s1_loop.wav ../data/s1_loop.wav'
+logAndRun 'diff s1_loop.wav ../data/s1_loop.back.wav'
 test
 
-logAndRun 'diff s2_loop.wav ../data/s2_loop.wav'
+logAndRun 'diff s2_loop.wav ../data/s2_loop.back.wav'
 test
 
 logAndRun '$srcdir/../../src/emu4bm -X ../data/emu4_test_add_sample_2'
 test
 
-logAndRun 'diff 001-s1_loop.wav ../data/s1_loop.wav'
+logAndRun 'diff 001-s1_loop.wav ../data/s1_loop.back.wav'
 test
 
-logAndRun 'diff 002-s2_loop.wav ../data/s2_loop.wav'
+logAndRun 'diff 002-s2_loop.wav ../data/s2_loop.back.wav'
 test
 
 cleanUp
