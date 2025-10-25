e41885120edc5c8b1a24ada40d1bcd2d52e80fbf
dagargo <dagargo@gmail.com>
2025-10-25 12:03:02 +0200

    Fix emu3 edit parameter test

--- test/emu3_test_edit_parameter.sh
+++ test/emu3_test_edit_parameter.sh
@@ -2,27 +2,11 @@
 
 . $srcdir/test_common.sh
 
-TEST_BANK_NAME=$srcdir/emu3_test_add_zone
+TEST_BANK_NAME=$srcdir/emu3_test_edit_parameter
 
 cleanUp
 
-logAndRun '$srcdir/../src/emu3bm -n $TEST_BANK_NAME'
-test
-
-logAndRun '$srcdir/../src/emu3bm -p "P0" $TEST_BANK_NAME'
-test
-logAndRun 'diff $TEST_BANK_NAME data/emu3_test_add_zone_1'
-test
-
-logAndRun '$srcdir/../src/emu3bm -s data/s1.wav $TEST_BANK_NAME'
-test
-logAndRun 'diff $TEST_BANK_NAME data/emu3_test_add_zone_2'
-test
-
-logAndRun '$srcdir/../src/emu3bm -e 0 -Z 1,pri,20,15,26 $TEST_BANK_NAME'
-test
-logAndRun 'diff $TEST_BANK_NAME data/emu3_test_add_zone_3'
-test
+cp data/emu3_test_add_zone_3 $TEST_BANK_NAME
 
 logAndRun '$srcdir/../src/emu3bm -e 0 -q 25 -c 200 $TEST_BANK_NAME'
 test
