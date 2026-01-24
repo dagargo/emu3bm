5432323c729577a8bbf4021cfb31780204d18450
dagargo <dagargo@gmail.com>
2026-01-24 12:19:02 +0100

    Add SFZ tests

--- test/emu3_test_add_sfz.sh
+++ test/emu3_test_add_sfz.sh
@@ -7,9 +7,43 @@ TEST_BANK_NAME=$srcdir/emu3_test_add_sfz
 cleanUp
 
 logAndRun '$srcdir/../src/emu3bm -n $TEST_BANK_NAME'
+logAndRun '$srcdir/../src/emu3bm -S data/test1.sfz $TEST_BANK_NAME'
+test
+logAndRun 'diff $TEST_BANK_NAME data/emu3_test_add_sfz_1'
 test
 
-logAndRun '$srcdir/../src/emu3bm -S data/test_grammar.sfz $TEST_BANK_NAME'
+logAndRun '$srcdir/../src/emu3bm -n $TEST_BANK_NAME'
+logAndRun '$srcdir/../src/emu3bm -S data/test2.sfz $TEST_BANK_NAME'
 test
+logAndRun 'diff $TEST_BANK_NAME data/emu3_test_add_sfz_2'
+test
+
+logAndRun '$srcdir/../src/emu3bm -n $TEST_BANK_NAME'
+logAndRun '$srcdir/../src/emu3bm -S data/test3.sfz $TEST_BANK_NAME'
+test
+logAndRun 'diff $TEST_BANK_NAME data/emu3_test_add_sfz_3'
+test
+
+logAndRun '$srcdir/../src/emu3bm -n $TEST_BANK_NAME'
+logAndRun '$srcdir/../src/emu3bm -S data/test4.sfz $TEST_BANK_NAME'
+testError
+
+logAndRun '$srcdir/../src/emu3bm -n $TEST_BANK_NAME'
+logAndRun '$srcdir/../src/emu3bm -S data/test5.sfz $TEST_BANK_NAME'
+testError
+
+logAndRun '$srcdir/../src/emu3bm -n $TEST_BANK_NAME'
+logAndRun '$srcdir/../src/emu3bm -S data/test6.sfz $TEST_BANK_NAME'
+test
+logAndRun 'diff $TEST_BANK_NAME data/emu3_test_add_sfz_6'
+test
+
+logAndRun '$srcdir/../src/emu3bm -n $TEST_BANK_NAME'
+logAndRun '$srcdir/../src/emu3bm -S data/test7.sfz $TEST_BANK_NAME'
+test
+logAndRun 'diff $TEST_BANK_NAME data/emu3_test_add_sfz_7'
+test
+
+rm $TEST_BANK_NAME_PRISTINE
 
 cleanUp
